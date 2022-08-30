f8bbc30396a8ef0be02e2b1d395064f95445c10f
Leonardo Cerchioni <57871388+djtrexi@users.noreply.github.com>
2022-08-30 14:37:19 +0200

    first commit of day 2022-08-30

---
+++ sign.php
@@ -0,0 +1,73 @@
+<?php
+  require("connessione.php");
+?>
+<!DOCTYPE html>
+<html lang = "en">
+  <head>
+    <title>Sign</title>
+  </head>
+  <body>
+    <div id = "header">
+      <header>
+        <div id = "title">
+          <h1>Sign</h1>
+        </div>
+      </header>
+    </div>
+    <div>
+      <main>
+        <section>
+            <?php
+              if(!isset($_POST['nome']) ||
+                !isset($_POST['cognome']) || 
+                !isset($_POST['email']) || 
+                !isset($_POST['password'])){
+            ?>
+                <article>
+                  <div id = "formNewClient">
+                    <form method = "POST" action = "sign.php">
+                      Nome <input type = "text" name = "nome">
+                      <br><br>
+                      Cognome <input type = "text" name = "cognome">
+                      <br><br>
+                      Email <input type = "email" name = "email">
+                      <br><br>
+                      Password <input type = "password" name = "password">
+                      <br><br>
+                      <input type = "submit" value = "Sign">
+                      <input type = "reset" value = "Reset">
+                    </form>
+                  </div>
+                </article>
+            <?php
+              }
+              else{
+            ?>
+            <?php
+                $nome = $_POST['nome'];
+                $cognome = $_POST['cognome'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                if(strlen($nome) == 0 &&
+                   strlen($cognome) == 0 &&
+                   strlen($email) == 0 &&
+                   strlen($password) == 0){
+                    $password = crypt($password, 0);
+                    $query = "SELECT *
+                              FROM utenti
+                              WHERE email = '$email'";
+                    $result = mysqli_query($connessione, $query);
+                    if(mysqli_num_rows($result) == 0){
+
+                    }
+                    else{
+
+                    }
+                }
+              }
+            ?>
+</section>
+      </main>
+    </div>
+  </body>    
+</html>
