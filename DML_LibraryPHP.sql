9fa5d8a70355803372980d2772fbaf37e51f8588
Leonardo Cerchioni <57871388+djtrexi@users.noreply.github.com>
2022-09-21 15:00:43 +0200

    DML code

---
+++ DML_LibraryPHP.sql
@@ -0,0 +1,19 @@
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
+
+INSERT INTO libri(titolo, casaeditrice, edizione, annodipubblicazione, prezzo) VALUES("Diabolik", "Astorina", "2", "1999-05-26", 5.50)
