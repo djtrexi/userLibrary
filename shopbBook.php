d7a2719f7a9107fe52a61b486fb1976a27669b90
Leonardo Cerchioni <57871388+djtrexi@users.noreply.github.com>
2022-09-23 17:03:14 +0200

    commit shopBook for the complete code

--- shopbBook.php
+++ shopbBook.php
@@ -3,5 +3,20 @@
   require('connessione.php');
 ?>
 <?php
-  
+  $idLibro = $_GET['idLibro'];
+  $idUtente = $_SESSION['idUtente'];
+  $query = "UPDATE aggiunta
+            SET acquistato = 1
+            WHERE codicelibri = '$idLibro' AND codiceutente = '$idUtente";
+  $result = mysqli_query($connessione, $query);
+  if($result){
+    mysqli_free_result($result);
+    header('Location: http://localhost/LibraryPHP/viewBooksNotShop.php/');
+  }
+  else{
+    $error = true;
+    $_SESSION['messaggio_errore'] = $error;
+    mysqli_free_result($result);
+    header('Location: http://localhost/LibraryPHP/infoBook.php/');
+  }
 ?>
