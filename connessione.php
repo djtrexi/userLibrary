65de5a6ca14f69a5a4615be9357b9e49e0a89b2a
Leonardo Cerchioni <57871388+djtrexi@users.noreply.github.com>
2022-09-24 13:27:15 +0200

    update file

--- connessione.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $connessione = mysqli_connect("localhost", "root", "", "bibloteca");
-  
-  if(mysqli_connect_errno()){
-    echo "<div>";
-      echo "<h3>La connessione al database è falita</h3>" .mysqli_connect_error();
-    echo "</div>";
-  }
-?>
